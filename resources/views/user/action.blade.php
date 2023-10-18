b1c469c311355e5cbf1f04473cd76beb9f6ab25f
Dhimas Tri Cahya <15051973-dhimastric@users.noreply.gitlab.com>
2023-10-18 16:22:50 +0700

    Initial Project + Show in DataTables

---
+++ resources/views/user/action.blade.php
@@ -0,0 +1,2 @@
+<a data-id="{{ $data->id }}" class="btn btn-warning btn-sm">Edit</a>
+<a data-id="{{ $data->id }}" class="btn btn-danger btn-sm">Delete</a>
---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Challenge</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="//cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
+</head>
+
+<body class="bg-light">
+    <div class="container">
+        <div class="my-4 p-4 bg-body rounded shadow-md">
+            {{-- Button Modal --}}
+            <button type="button" class="btn btn-primary" id="button-modal">
+                + Tambah Data
+            </button>
+
+            {{-- Table --}}
+            <div class="row justify-content-center">
+                <div class="mt-3">
+                    <table class="table table-striped" id="userTable">
+                        <thead>
+                            <tr class="table-dark">
+                                <th scope="col">#</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Email</th>
+                                <th scope="col">Role</th>
+                                <th scope="col">Action</th>
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
+            </div>
+
+            {{-- Modal --}}
+            <div class="modal fade" id="modal-form" tabindex="-1" aria-hidden="true">
+                <div class="modal-dialog modal-lg modal-dialog-centered modal-dialog-scrollable">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title">Modal title</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <p>Modal body text goes here.</p>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    {{-- ========================================================================================================= --}}
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+        integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous">
+    </script>
+    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
+        crossorigin="anonymous"></script>
+    <script src="//cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#userTable').DataTable({
+                processing: true,
+                serverside: true,
+                ajax: "{{ url('userAjax') }}",
+                columns: [{
+                    data: 'DT_RowIndex',
+                    name: 'DT_RowIndex',
+                    orderable: false,
+                    searchable: false
+                }, {
+                    data: 'name',
+                    name: 'Name',
+                }, {
+                    data: 'email',
+                    name: 'Email',
+                }, {
+                    data: 'role',
+                    name: 'Role',
+                }, {
+                    data: 'action',
+                    name: 'Action',
+                }]
+            });
+        });
+
+        $('body').on('click', '#button-modal', function(e) {
+            e.preventDefault();
+            $('#modal-form').modal('show');
+        });
+    </script>
+</body>
+
+</html>
